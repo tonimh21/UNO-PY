f97542ed17c8abbca0d0ee016f59e110b7e3ac96
Mauricio Rivera Motta <mriveram@cifpfbmoll.eu>
2023-01-11 21:04:00 +0100

    He modificado el codigo de Toni y he añadido la funcion de las manos de los jugadores

--- UNO.py
+++ UNO.py
@@ -11,7 +11,7 @@ def CrearMazo():
     mazo = []
 
     #Cartas del juego ordenadas según su tipo
-    colores = ["Rojo", "Azul", "Amarillo","Verde"]
+    colores = ["Rojo", "Azul", "Amarillo", "Verde"]
     normales = [0,1,2,3,4,5,6,7,8,9,"+2","BLOQUEO","REVERSO"]
     especiales = ["CAMBIO DE COLOR","+4"]
 
@@ -30,11 +30,45 @@ def CrearMazo():
     
     return mazo
 
+#Función para mezclar la baraja ya creada
 def mezclarBaraja(mazo):
+
+    #Bucle para sacar las cartas aleatoriamente y girarlas para mezclarlas aún más
     for i in range(len(mazo)):
         aleatorio=random.randint(0,107)
         mazo[i], mazo[aleatorio] = mazo[aleatorio], mazo[i]
+        
     return mazo
+
+#Función para crear los mazos a todos los jugadores
+def pintarCartas(numCartas):
+
+    cartas_pintadas = []
+
+    #Usamos 'pop' para que saque un valor aleatorio del mazo creado, y que a la vez lo elimine
+    for i in range(numCartas):
+        cartas_pintadas.append(mazoUno.pop(0))
+
+    return cartas_pintadas
+
+#Mostramos por pantalla los nombres de los jugadores y la baraja que le corresponde a cada uno
+def mano_del_jugador(jugador,ManoJugador):
+	
+	print("Jugador: {}".format(jugador+1))
+	print("Su mano: ")
+	print("-----------------------------------")
+	for carta in ManoJugador:
+		print(carta)
+	print("")
 
 mazoUno = CrearMazo()
 mazoUno = mezclarBaraja(mazoUno)
+
+jugadores = []
+num_jugadores=int(input("Cuántos jugadores sois: "))
+for i in range(num_jugadores):
+    jugadores.append(pintarCartas(7))
+
+turno_jugador=0
+direccion_juego=1
+juego=True
